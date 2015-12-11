638e7a25f6548ce8e57a0f52db890d6d1b275987
bob <bob@1ayule.com>
2015-12-11 10:19:43 +0800

    add for logic

--- CatchPic.py
+++ CatchPic.py
@@ -1,17 +1,18 @@
 import requests
 from lxml import etree
 
-a = requests.get("http://www.wetu.me/search/nature%20outdoor")
+def downPic(n):
+    i = 1
+    a = requests.get("http://www.wetu.me/search?keyword=city%20view&pageindex="+str(n))
+    selector = etree.HTML(a.text)
+    content = selector.xpath('//*[@id="gallery-list"]/li/a/img/@data-original')
+    for each in content:
+        print('downloading:' + each)
+        pic = requests.get(each)
+        fp = open('/Users/1akang/Desktop/pictures/city' + str(n)+'-'+str(i) + '.jpg', 'wb')
+        fp.write(pic.content)
+        fp.close()
+        i += 1
 
-selector = etree.HTML(a.text)
-
-content = selector.xpath('//*[@id="gallery-list"]/li/a/img/@data-original')
-
-i = 1
-for each in content:
-    print('downloading:' + each)
-    pic = requests.get(each)
-    fp = open('/Users/1akang/Desktop/pictures/' + str(i) + '.jpg', 'wb')
-    fp.write(pic.content)
-    fp.close()
-    i += 1
+for j in range(1,21):
+    downPic(j)
